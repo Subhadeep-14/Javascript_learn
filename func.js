ad72a16caeeb450f6ece7a872d699c03c67e8612
Subhadeep-14 <csubhadeep35@gmail.com>
2025-12-08 20:36:57 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,17 @@
+console.log(swap(4,5));
+function swap( a, b){
+    a=a+b
+    b=a-b;
+    a=a-b;
+    return [a,b];
+
+}
+
+const swap1=(a,b)=>{
+    a=a+b
+    b=a-b;
+    a=a-b;
+    return [a,b];
+
+}
+console.log(swap1(1,14));
