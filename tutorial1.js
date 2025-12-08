a15cc651b5f718c395ac0dd6bf3230d9e22bafab
Subhadeep-14 <csubhadeep35@gmail.com>
2025-12-08 10:28:06 +0530

    Add files via upload

---
+++ tutorial1.js
@@ -0,0 +1,21 @@
+var a="5";
+a=a+6;
+console.log(a); 
+console.log(typeof a);
+const item={
+    name:"Book",
+    age:20
+}
+item.name="Pencil";
+item.age=25;
+console.log(item);
+item.class=7;
+console.log(item);
+const mean={
+    Abstruse: "Difficult to understand; obscure",
+    Alacrity: "Eagerness and enthusiasm",
+    Cacophony: "A harsh, discordant mixture of sounds.",
+    Ephemeral: "Lasting for a very short time.",
+    Pernicious: "Having a harmful effect, especially in a subtle or gradual way. "
+}
+console.log(mean);
