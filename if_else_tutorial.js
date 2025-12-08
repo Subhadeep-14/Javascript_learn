94a955786f2bab92c483f9b56283e590ef18d4d6
Subhadeep-14 <csubhadeep35@gmail.com>
2025-12-08 10:43:07 +0530

    Add files via upload

---
+++ if_else_tutorial.js
@@ -0,0 +1,33 @@
+var a=19;
+if((a>=10)&& (a<=20)){
+    console.log("The value of a lies between 10 and 20");
+}
+else
+{
+    console.log("The value of a does not lie between 10 and 20");
+}
+
+if((a%2==0)&& (a%3==0))
+{
+    console.log("The value of a is divisible by both 2 and 3");
+
+}
+else
+{
+    console.log("The value of a is not divisible by both 2 and 3");
+}
+if((a%2==0)||(a%3==0))
+{
+    console.log("The value of a is divisible by either 2 or 3");            
+}
+else
+{
+    console.log("The value of a is not divisible by either 2 or 3");
+}
+let age=25;
+if(age<18){
+    console.log("You cannot drive");
+}
+else{
+    console.log("You can drive");
+}
