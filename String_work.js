ea924531ffe36d1b95c1506dd93e81cf31db8385
Subhadeep-14 <csubhadeep35@gmail.com>
2025-12-09 10:22:37 +0530

    Add files via upload

---
+++ String_work.js
@@ -0,0 +1,7 @@
+console.log("har\"".length)
+var c="HARRY POTTER  ";
+c=c.toLowerCase();
+console.log(c);
+var str="Please give Rs 1000";
+console.log(str.slice(15));
+console.log(str.replace(str[3],"x"));
